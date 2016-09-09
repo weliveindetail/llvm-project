7cc993eacafe930a0534c651251d15a4453c1f77
Frederich Munch <marsupial@users.noreply.github.com>
2016-09-09 16:48:28 -0400

    CLING: Move cling DeclUnloader and ParserStateRAII into cling namespace.

--- clang/include/clang/Parse/Parser.h
+++ clang/include/clang/Parse/Parser.h
@@ -30,6 +30,8 @@
 #include <memory>
 #include <stack>
 
+namespace cling { class ParserStateRAII; }
+
 namespace clang {
   class PragmaHandler;
   class Scope;
@@ -65,6 +67,7 @@ class Parser : public CodeCompletionHandler {
   friend class ObjCDeclContextSwitch;
   friend class ParenBraceBracketBalancer;
   friend class BalancedDelimiterTracker;
+  friend class ::cling::ParserStateRAII;
 
   Preprocessor &PP;
 
