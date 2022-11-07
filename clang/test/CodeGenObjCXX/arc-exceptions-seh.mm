a67be5a56c223e0d179e8de3787297746f06a1f0
Stefan Gränitz <stefan.graenitz@gmail.com>
2022-11-07 12:23:58 +0100

    [CGObjC] Open Cleanup scope before SaveAndRestore CurrentFuncletPad so we can push CatchRetScope early
    Following implementation in CodeGenFunction::ExitCXXTryStmt() in clang/lib/CodeGen/CGException.cpp

--- clang/test/CodeGenObjCXX/arc-exceptions-seh.mm
+++ clang/test/CodeGenObjCXX/arc-exceptions-seh.mm
@@ -3,27 +3,48 @@
 // WinEH requires funclet tokens on nounwind intrinsics if they can lower to
 // regular function calls in the course of IR transformations.
 //
-// This is the case for ObjC ARC runtime intrinsics. Test that clang emits the
-// funclet tokens for llvm.objc.retain and llvm.objc.storeStrong and that they
-// refer to their catchpad's SSA value.
+// This is the case for ObjC ARC runtime intrinsics. Test that:
+// (1) Clang emits funclet tokens for llvm.objc.* calls
+// (2) All such calls are between catchpad and catchret/unreachable
+// (3) Funclet tokens refer to their catchpad's SSA value
 
 @class Ety;
 void opaque(void);
-void test_catch_with_objc_intrinsic(void) {
+void do_rethrow() {
   @try {
     opaque();
   } @catch (Ety *ex) {
-    // Destroy ex when leaving catchpad. This emits calls to intrinsic functions
-    // llvm.objc.retain and llvm.objc.storeStrong
+    @throw ex;
+  }
+}
+void test_catch_with_objc_intrinsic(void) {
+  @try {
+    do_rethrow();
+  } @catch (Ety *ex) {
+    // Destroy ex when leaving catchpad. This would emit calls to two
+    // intrinsic functions: llvm.objc.retain and llvm.objc.storeStrong
   }
 }
 
-// CHECK-LABEL: define{{.*}} void {{.*}}test_catch_with_objc_intrinsic
+// CHECK-LABEL: do_rethrow
 //                ...
 // CHECK:       catch.dispatch:
-// CHECK-NEXT:    [[CATCHSWITCH:%[0-9]+]] = catchswitch within none
+// CHECK-NEXT:    [[CATCHSWITCH:%[0-9]+]] = catchswitch within none [label %catch]
 //                ...
 // CHECK:       catch:
 // CHECK-NEXT:    [[CATCHPAD:%[0-9]+]] = catchpad within [[CATCHSWITCH]]
-// CHECK:         {{%[0-9]+}} = call {{.*}} @llvm.objc.retain{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
-// CHECK:         call {{.*}} @llvm.objc.storeStrong{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:         @llvm.objc.retain{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:         @llvm.objc.retainAutorelease{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:         @objc_exception_throw{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:         unreachable
+
+// CHECK-LABEL: test_catch_with_objc_intrinsic
+//                ...
+// CHECK:       catch.dispatch:
+// CHECK-NEXT:    [[CATCHSWITCH:%[0-9]+]] = catchswitch within none [label %catch]
+//                ...
+// CHECK:       catch:
+// CHECK-NEXT:    [[CATCHPAD:%[0-9]+]] = catchpad within [[CATCHSWITCH]]
+// CHECK:         @llvm.objc.retain{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:         @llvm.objc.storeStrong{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:         catchret from [[CATCHPAD]] to label %catchret.dest
