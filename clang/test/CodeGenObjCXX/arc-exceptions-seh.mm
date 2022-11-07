03c286edd698716524adeac1bc53a5aa37b2bdd7
Stefan Gränitz <stefan.graenitz@gmail.com>
2022-11-07 12:23:58 +0100

    [CGObjC] Open cleanup scope before SaveAndRestore CurrentFuncletPad and push CatchRetScope early
    Following implementation in CodeGenFunction::ExitCXXTryStmt() in clang/lib/CodeGen/CGException.cpp

--- clang/test/CodeGenObjCXX/arc-exceptions-seh.mm
+++ clang/test/CodeGenObjCXX/arc-exceptions-seh.mm
@@ -7,23 +7,42 @@
 // funclet tokens for llvm.objc.retain and llvm.objc.storeStrong and that they
 // refer to their catchpad's SSA value.
 
-@class Ety;
-void opaque(void);
-void test_catch_with_objc_intrinsic(void) {
+void do_something();
+void may_throw(id);
+
+void try_catch_with_objc_intrinsic() {
+  id ex;
   @try {
-    opaque();
-  } @catch (Ety *ex) {
-    // Destroy ex when leaving catchpad. This emits calls to intrinsic functions
-    // llvm.objc.retain and llvm.objc.storeStrong
+    may_throw(ex);
+  } @catch (id ex_caught) {
+    do_something();
+    may_throw(ex_caught);
   }
 }
 
-// CHECK-LABEL: define{{.*}} void {{.*}}test_catch_with_objc_intrinsic
-//                ...
-// CHECK:       catch.dispatch:
-// CHECK-NEXT:    [[CATCHSWITCH:%[0-9]+]] = catchswitch within none
-//                ...
-// CHECK:       catch:
-// CHECK-NEXT:    [[CATCHPAD:%[0-9]+]] = catchpad within [[CATCHSWITCH]]
-// CHECK:         {{%[0-9]+}} = call {{.*}} @llvm.objc.retain{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
-// CHECK:         call {{.*}} @llvm.objc.storeStrong{{.*}} [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK-LABEL:   try_catch_with_objc_intrinsic
+//
+// CHECK:         catch.dispatch:
+// CHECK-NEXT:      [[CATCHSWITCH:%[0-9]+]] = catchswitch within none [label %catch]
+//
+// All calls within a catchpad must have funclet tokens that refer to the
+// enclosing catchpad:
+// CHECK:         catch:
+// CHECK-NEXT:      [[CATCHPAD:%[0-9]+]] = catchpad within [[CATCHSWITCH]]
+// CHECK:           call
+// CHECK:           @llvm.objc.retain
+// CHECK:           [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:           call
+// CHECK:           do_something
+// CHECK:           [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:           call
+// CHECK:           may_throw
+// CHECK:           [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:           call
+// CHECK:           @llvm.objc.storeStrong
+// CHECK:           [ "funclet"(token [[CATCHPAD]]) ]
+// CHECK:           catchret from [[CATCHPAD]] to label %catchret.dest
+//
+// This block exists and it's empty:
+// CHECK:         catchret.dest:
+// CHECK-NEXT:      br label %eh.cont
