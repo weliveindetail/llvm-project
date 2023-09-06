f7b9a37c773ede48e4eea6a2c6ef6739f6daf277
Stefan Gränitz <stefan.graenitz@gmail.com>
2023-09-06 16:44:57 +0200

    [wip] Polish tests

--- llvm/test/ExecutionEngine/JITLink/AArch32/ELF_static_arm_reloc.s
+++ llvm/test/ExecutionEngine/JITLink/AArch32/ELF_static_arm_reloc.s
@@ -1,56 +1,62 @@
-# RUN: llvm-mc -triple=thumbv7-linux-gnueabi -arm-add-build-attributes \
-# RUN:		-filetype=obj -o %t.o %s
-# RUN: llvm-objdump -r %t.o | FileCheck %s
+# RUN: llvm-mc -triple=armv7-linux-gnueabi -arm-add-build-attributes \
+# RUN:	       -filetype=obj -o %t.o %s
+# RUN: llvm-objdump -r %t.o | FileCheck --check-prefix=CHECK-TYPE %s
+# RUN: llvm-objdump --disassemble %t.o | FileCheck --check-prefix=CHECK-INSTR %s
 # RUN: llvm-jitlink -noexec -slab-address 0x76ff0000 -slab-allocate 10Kb \
 # RUN:              -slab-page-size 4096 -show-entry-es -check %s %t.o
 
 	.text
-	.thumb
-	.p2align	1
+	.syntax unified
 
-	.globl	main
-	.type	main,%function
-	.code	16
-	.thumb_func
-main:
-	bx	lr
-	.size	main,	.-main
-
-# CHECK: {{[0-9a-f]+}} R_ARM_THM_CALL call_target
+# CHECK-TYPE: {{[0-9a-f]+}} R_ARM_CALL call_target
+# CHECK-INSTR: 	00000000 <call_site>:
+# CHECK-INSTR: 	       0: ebfffffe      bl      0x0 <call_site>
+# CHECK-INSTR: 	00000004 <call_target>:
+# CHECK-INSTR: 	       4: e12fff1e      bx      lr
 # jitlink-check: decode_operand(call_site, 0) = call_target - next_pc(call_site)
 	.globl	call_site
 	.type	call_site,%function
-	.code	16
-	.thumb_func
+	.p2align	2
+	.code	32
 call_site:
 	bl	call_target
 	.size	call_site,	.-call_site
 
 	.globl	call_target
 	.type	call_target,%function
-	.code	16
-	.thumb_func
+	.p2align	2
+	.code	32
 call_target:
 	bx	lr
 	.size	call_target,	.-call_target
 
-# CHECK: {{[0-9a-f]+}} R_ARM_THM_JUMP24 jump24_target
+# CHECK-TYPE: {{[0-9a-f]+}} R_ARM_JUMP24 jump24_target
+# CHECK-INSTR: 	00000008 <jump24_site>:
+# CHECK-INSTR: 	       8: eafffffe      b       0x8 <jump24_site>
+# CHECK-INSTR: 	0000000c <jump24_target>:
+# CHECK-INSTR: 	       c: e12fff1e      bx      lr
 # jitlink-check: decode_operand(jump24_site, 0) = jump24_target - next_pc(jump24_site)
 	.globl	jump24_site
 	.type	jump24_site,%function
-	.code	16
-	.thumb_func
+	.p2align	2
+	.code	32
 jump24_site:
 	b.w	jump24_target
 	.size	jump24_site,	.-jump24_site
 
 	.globl	jump24_target
 	.type	jump24_target,%function
-	.code	16
-	.thumb_func
+	.p2align	2
+	.code	32
 jump24_target:
 	bx	lr
 	.size	jump24_target,	.-jump24_target
 
-
-// jitlink-check: (main + *{4}(main))[31:0] = target_jump24
+# Empty main function for jitlink to be happy
+	.globl	main
+	.type	main,%function
+	.p2align	2
+	.code	32
+main:
+	bx	lr
+	.size	main,	.-main
