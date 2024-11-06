1d7ed21a544029a2d03425e49cb1a4afd2f5b9d5
Stefan Gränitz <stefan.graenitz@gmail.com>
2024-11-06 19:40:57 +0100

    Patch test for v1-4 (wip)

--- lldb/test/Shell/SymbolFile/DWARF/x86/member-pointers.cpp
+++ lldb/test/Shell/SymbolFile/DWARF/x86/member-pointers.cpp
@@ -14,7 +14,7 @@
 // CHECK-GNU: (void (Virtual4::*)()) v4 = 0x00000000000000000000000000000000
 
 // Microsoft ABI:
-// RUN: %clang_cl --target=x86_64-windows-msvc -c -gdwarf -o %t_win.obj -- %s
+// RUN: %clang_cl --target=x86_64-windows-msvc -c -gdwarf -o %t_win.obj /GS- -- %s
 // RUN: lld-link /out:%t_win.exe %t_win.obj /entry:main /debug /nodefaultlib
 // RUN: %lldb -f %t_win.exe -b -o "target variable s1 s2 m1 m2 v1 v2 v3 v4" | FileCheck --check-prefix=CHECK-MSVC %s
 //
@@ -22,10 +22,10 @@
 // CHECK-MSVC: (void (Single2::*)()) s2 = 0x0000000000000000
 // CHECK-MSVC: (void (Multiple1::*)()) m1 = 0x00000000000000000000000000000000
 // CHECK-MSVC: (void (Multiple2::*)()) m2 = 0x00000000000000000000000000000000
-// CHECK-MSVC: error: Unable to determine byte size.
-// CHECK-MSVC: error: Unable to determine byte size.
-// CHECK-MSVC: error: Unable to determine byte size.
-// CHECK-MSVC: error: Unable to determine byte size.
+// CHECK-MSVC: (void (Virtual1::*)()) v1 = 0xffffffff000000000000000000000000
+// CHECK-MSVC: (void (Virtual2::*)()) v2 = 0xffffffff000000000000000000000000
+// CHECK-MSVC: (void (Virtual3::*)()) v3 = 0xffffffff000000000000000000000000
+// CHECK-MSVC: (void (Virtual4::*)()) v4 = 0xffffffff000000000000000000000000
 
 // clang-format off
 struct Single1 { void s1() {} };
@@ -50,6 +50,10 @@ void (Virtual3::*v3)() = nullptr;
 void (Virtual4::*v4)() = nullptr;
 
 int main(int argc, char *argv[]) {
-  int sum = sizeof(Single2) + sizeof(Multiple2) + sizeof(Virtual1) + sizeof(Virtual2) + sizeof(Virtual3) + sizeof(Virtual4);
+  Virtual1 vi1;
+  Virtual2 vi2;
+  Virtual3 vi3;
+  Virtual4 vi4;
+  int sum = sizeof(Single2) + sizeof(Multiple2);
   return argc < sum ? 0 : 1;
 }
