b5b0b640dc789f762bb912cbcf936d71919d639c
Stefan Gränitz <stefan.graenitz@gmail.com>
2025-11-13 09:43:20 +0100

    [lli] Enforce --jit-linker-kind=runtimedyld on platforms that default to jitlink

--- llvm/tools/lli/lli.cpp
+++ llvm/tools/lli/lli.cpp
@@ -1021,23 +1021,32 @@ static int runOrcJIT(const char *ProgName) {
     llvm_unreachable("Unrecognized platform value");
   }
 
-  std::unique_ptr<orc::ExecutorProcessControl> EPC = nullptr;
-  if (JITLinker == JITLinkerKind::JITLink) {
-    EPC = ExitOnErr(orc::SelfExecutorProcessControl::Create(
-        std::make_shared<orc::SymbolStringPool>()));
-
+  switch (JITLinker) {
+  case JITLinkerKind::JITLink:
     Builder.getJITTargetMachineBuilder()
         ->setRelocationModel(Reloc::PIC_)
         .setCodeModel(CodeModel::Small);
     Builder.setObjectLinkingLayerCreator([&](orc::ExecutionSession &ES) {
       return std::make_unique<orc::ObjectLinkingLayer>(ES);
     });
+    break;
+  case JITLinkerKind::RuntimeDyld:
+    Builder.setObjectLinkingLayerCreator([&](orc::ExecutionSession &ES) {
+      return std::make_unique<orc::RTDyldObjectLinkingLayer>(ES, [](const MemoryBuffer &) {
+        return std::make_unique<SectionMemoryManager>();
+      });
+    });
+    break;
+  case JITLinkerKind::Default:
+    // Let LLJITBuilder decide
+    break;
   }
 
   auto J = ExitOnErr(Builder.create());
 
   auto *ObjLayer = &J->getObjLinkingLayer();
   if (auto *RTDyldObjLayer = dyn_cast<orc::RTDyldObjectLinkingLayer>(ObjLayer)) {
+    JITLinker = JITLinkerKind::RuntimeDyld;
     RTDyldObjLayer->registerJITEventListener(
         *JITEventListener::createGDBRegistrationListener());
 #if LLVM_USE_OPROFILE
@@ -1052,6 +1061,8 @@ static int runOrcJIT(const char *ProgName) {
     RTDyldObjLayer->registerJITEventListener(
         *JITEventListener::createPerfJITEventListener());
 #endif
+  } else if (isa<orc::ObjectLinkingLayer>(ObjLayer)) {
+    JITLinker = JITLinkerKind::JITLink;
   }
 
   if (PerModuleLazy)
@@ -1161,18 +1172,10 @@ static int runOrcJIT(const char *ProgName) {
   }
 
   // Resolve and run the main function.
+  using MainFnTy = int(int, char *[]);
   auto MainAddr = ExitOnErr(J->lookup(EntryFunc));
-  int Result;
-
-  if (EPC) {
-    // ExecutorProcessControl-based execution with JITLink.
-    Result = ExitOnErr(EPC->runAsMain(MainAddr, InputArgv));
-  } else {
-    // Manual in-process execution with RuntimeDyld.
-    using MainFnTy = int(int, char *[]);
-    auto MainFn = MainAddr.toPtr<MainFnTy *>();
-    Result = orc::runAsMain(MainFn, InputArgv, StringRef(InputFile));
-  }
+  auto MainFn = MainAddr.toPtr<MainFnTy *>();
+  int Result = orc::runAsMain(MainFn, InputArgv, StringRef(InputFile));
 
   // Wait for -entry-point threads.
   for (auto &AltEntryThread : AltEntryThreads)
